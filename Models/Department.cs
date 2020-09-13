adfee71621bb2a4c5b8ca714ec246a200f061d73
sohaib jameel <sohiabjameel3@gmail.com>
2020-09-13 13:46:17 +0500

    first commit

---
+++ Models/Department.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SohaibJameel.Models
+{
+    public class Department
+    {
+        public int ID { get; set; }
+        public string name { get; set; }
+        public string location { get; set; }
+        public int no_of_fculty { get; set; }
+    }
+
+    public class DepartmentDBContext : DbContext
+    {
+        public DbSet<Department> Departments { get; set; }
+    }
+}
